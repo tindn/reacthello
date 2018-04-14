41606d4523a50d95467af6cfca440b705b0c30bb
Tin Nguyen <tin.nguyen1@ge.com>
2018-04-14 09:45:26 -0400

    redux wiring

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import './index.css';
+import Reducers from './state/reducers';
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  Reducers,
+  window['__REDUX_DEVTOOLS_EXTENSION__'] &&
+    window['__REDUX_DEVTOOLS_EXTENSION__']()
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
